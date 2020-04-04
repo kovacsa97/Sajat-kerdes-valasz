ad4ee901790598151ee3b45f3c3ab15c084fedb5
kovacs.adam@autsoft.hu <kovacs.adam@autsoft.hu>
2020-04-04 18:01:43 +0200

    lol

---
+++ Cognitive.LUIS.Programmatic/Models/Publish/Publish.cs
@@ -0,0 +1,20 @@
+using Newtonsoft.Json;
+using System;
+ 
+ namespace Cognitive.LUIS.Programmatic.Models
+ {
+     public class Publish
+     {
+        public int MyProperty { get; set; }
+
+        public string VersionId { get; set; }
+        public bool DirectVersionPublish { get; set; }
+        public string EndpointUrl { get; set; }
+        public bool IsStaging { get; set; }
+        public string AssignedEndpointKey { get; set; }
+        public string Region { get; set; }
+        public string EndpointRegion { get; set; }
+        public DateTime PublishedDateTime { get; set; }
+        public string FailedRegions { get; set; }
+    }
+ }
